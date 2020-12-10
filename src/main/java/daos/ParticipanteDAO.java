4fea52b767ae4cd332c26e02c11f8a6400f5b801
damianlips <damianlips87@gmail.com>
2020-12-10 01:13:28 -0300

    Manejo de mensaje en crearParticipante

--- src/main/java/daos/ParticipanteDAO.java
+++ src/main/java/daos/ParticipanteDAO.java
@@ -24,7 +24,7 @@ public class ParticipanteDAO {
 	}
 	
 	
-	public Boolean verificarExistencia(String nombre, Integer idCompetencia, String email) {
+	public Boolean verificarExistenciaNombre(String nombre, Integer idCompetencia) {
 		List<Participante> p1, p2;
 		EntityManager em	= factory.createEntityManager();
 		em.getTransaction().begin();
@@ -33,12 +33,19 @@ public class ParticipanteDAO {
 	      .setParameter(2, nombre)
 	      .getResultList();
 
+	    return (!p1.isEmpty());
+	}
+	
+	public Boolean verificarExistenciaMail(Integer idCompetencia, String email) {
+		List<Participante> p1, p2;
+		EntityManager em	= factory.createEntityManager();
+		em.getTransaction().begin();
 	    p2= em.createQuery("SELECT participante from Participante participante where participante.competencia.id = ?1 and participante.email = ?2")
 	      .setParameter(1, idCompetencia)
 	      .setParameter(2, email)
 	      .getResultList();
 		 em.getTransaction().commit();
 		 em.close();
-	    return (!p1.isEmpty() || !p2.isEmpty());
+	    return (!p2.isEmpty());
 	}
 }
--- src/main/java/gestores/GestorParticipante.java
+++ src/main/java/gestores/GestorParticipante.java
@@ -19,16 +19,29 @@ public class GestorParticipante {
 		gc = new GestorCompetencia();
 	}
 	
-	public void crearParticipante(ParticipanteDTO pdto) {
-		if(pd.verificarExistencia(pdto.getNombre(), pdto.getIdCompetencia(), pdto.getEmail())) {
-			//tirar excepcion o algo
+	public Mensaje crearParticipante(ParticipanteDTO pdto) {
+		Mensaje mensaje = new Mensaje();
+		if(pd.verificarExistenciaNombre(pdto.getNombre(), pdto.getIdCompetencia())) {
+			mensaje.setAccion(1);
+			mensaje.setTexto("Ya existe un participante con este nombre");
+			return mensaje;
+		}
+		if(pd.verificarExistenciaMail(pdto.getIdCompetencia(),pdto.getEmail())) {
+			mensaje.setAccion(2);
+			mensaje.setTexto("Ya existe un participante con este email");
+			return mensaje;
 		}
 		Competencia compe = gc.verificarCompetencia(pdto);
 		if(compe==null) {
-			//tirar excepcion o algo
+			mensaje.setAccion(3);
+			mensaje.setTexto("Ya no se puede agregar participantes a esta competencia");
+			return mensaje;
 		}
 		Participante p = new Participante(pdto.getNombre(), pdto.getEmail(), compe);
 		gc.agregarParticipante(p, compe);
+		mensaje.setAccion(0);
+		mensaje.setTexto("La operación ha culminado con éxito");
+		return mensaje;
 	}
 	
 	public List<ParticipanteDTO> getParticipantes(int idCompetencia){
