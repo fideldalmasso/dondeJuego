5a6365bc3ad22aaecaec06786ce59b0bd17b618f
JU4N98 <juanpablocabana2@gmail.com>
2020-12-09 23:41:49 -0300

    Corrijo error de mapeo.

--- src/main/java/daos/ParticipanteDAO.java
+++ src/main/java/daos/ParticipanteDAO.java
@@ -28,12 +28,12 @@ public class ParticipanteDAO {
 		List<Participante> p1, p2;
 		EntityManager em	= factory.createEntityManager();
 		em.getTransaction().begin();
-	    p1= em.createQuery("SELECT participante from Participante participante where participante.idCompetencia = ?1 and participante.nombre = ?2")
+	    p1= em.createQuery("SELECT participante from Participante participante where participante.competencia.id = ?1 and participante.nombre = ?2")
 	      .setParameter(1, idCompetencia)
 	      .setParameter(2, nombre)
 	      .getResultList();
 
-	    p2= em.createQuery("SELECT participante from Participante participante where participante.idCompetencia = ?1 and participante.email = ?2")
+	    p2= em.createQuery("SELECT participante from Participante participante where participante.competencia.id = ?1 and participante.email = ?2")
 	      .setParameter(1, idCompetencia)
 	      .setParameter(2, email)
 	      .getResultList();
